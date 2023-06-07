9870814bd2acf7a816fef5303a9ac9ebf562b7df
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-07 23:52:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,14 @@ provider "aws" {
   secret_key = "HBcjAF/Eqt2fGFcTUG29WyE9NE9r0QvkC9AR3iJa"
 }
 
-resource "aws_instance" "my_ec2_instance" {
-  ami                          = "ami-0261755bbcb8c4a84"
-  instance_type                = "t2.medium"
-  key_name                     = "project"
-  vpc_security_group_ids       = [aws_security_group.my_security_group.id]
-  subnet_id                    = aws_subnet.my_subnet.id
-  associate_public_ip_address  = true
-
-  user_data = <<-EOF
+resource "aws_instance" "my_ec2_instance" { 
+ ami = "ami-0261755bbcb8c4a84" 
+ instance_type = "t2.medium" 
+ key_name = "project" 
+ tags ={ 
+ name="Terraform hands-on" 
+} 
+   user_data = <<-EOF
     #!/bin/bash
     apt-get update -y
     apt-get install -y apt-transport-https ca-certificates curl software-properties-common
@@ -22,34 +21,4 @@ resource "aws_instance" "my_ec2_instance" {
     apt-get install -y docker-ce docker-ce-cli containerd.io
     usermod -aG docker ubuntu
   EOF
-
-  tags = {
-    Name = "my-instance"
-  }
 }
-
-resource "aws_security_group" "my_security_group" {
-  name        = "my-security-group"
-  description = "Allow all traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = "vpc-0815824eeaeccaab8"
-  cidr_block              = "10.0.0.0/24"  # Replace with a valid CIDR block within the VPC CIDR range
-  availability_zone       = "us-east-1a"   # Replace with your desired availability zone
-}
-
