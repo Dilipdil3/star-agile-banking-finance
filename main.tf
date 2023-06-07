34fce7d24ac2435603d5ea053e1fa13bcf1a951c
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-07 23:37:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ provider "aws" {
   secret_key = "HBcjAF/Eqt2fGFcTUG29WyE9NE9r0QvkC9AR3iJa"
 }
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-}
-
 resource "aws_instance" "my_ec2_instance" {
   ami                          = "ami-0261755bbcb8c4a84"
   instance_type                = "t2.medium"
