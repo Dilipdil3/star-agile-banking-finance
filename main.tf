37b076fb015d8b7c5a4fba580d9e2b86f448bea1
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-07 22:52:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ provider "aws" {
   secret_key = "HBcjAF/Eqt2fGFcTUG29WyE9NE9r0QvkC9AR3iJa"
 }
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-}
-
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
