dd6dfc015ff8791dfe5cec8fe51e5cd923abf82f
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-07 11:51:42 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,86 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA2L4SN5M7OUEGVIOG"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1"
+}
+
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "My Security Group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_network_interface" "my_network_interface" {
+  subnet_id    = aws_subnet.my_subnet.id
+  private_ips  = ["10.0.1.10"]
+}
+
+resource "aws_eip" "my_eip" {
+  vpc               = true
+  network_interface = aws_network_interface.my_network_interface.id
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.medium"
+  key_name               = "project"
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  subnet_id              = aws_subnet.my_subnet.id
+  associate_public_ip_address = true
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "sudo usermod -aG docker ubuntu"
+    ]
+  }
+}
