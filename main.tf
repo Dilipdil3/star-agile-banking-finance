3145bbf4d07ff378498d0425830ac6fc4ad5f1a8
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-07 21:30:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1a"
+  region     = "us-east-1"
   access_key = "AKIA2L4SN5M7HCPJSX64"
   secret_key = "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"
 }
@@ -19,7 +19,7 @@ resource "aws_route_table" "my_route_table" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1"
 }
 
 resource "aws_route_table_association" "my_route_table_association" {
