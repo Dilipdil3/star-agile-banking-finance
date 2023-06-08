e46f511055dabec43eeb4e0748c2779ef52d4beb
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-08 11:11:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = credentials("access_key")
-  secret_key = credentials("secret_key")
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "my_ec2_instance" { 
