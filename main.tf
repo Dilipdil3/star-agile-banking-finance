5064303618614c6580eac91419f9b94e6a4bb6f1
Dilipdil3 <129975032+Dilipdil3@users.noreply.github.com>
2023-06-20 10:51:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA2L4SN5M7HCPJSX64"
-  secret_key = "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"
 }
 
 resource "aws_instance" "my_ec2_instance" { 
